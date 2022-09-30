545ba96c423ace55dd780092bf52744f4fb155ec
BossBaby25 <meraj15-4213@diu.edu.bd>
2022-09-30 18:37:55 +0600

    first question

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,6 +4,9 @@ const Question = () => {
     return (
         <div>
             <h3>How does react work?</h3>
+            <p>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
+
+            
         </div>
     );
 };
