bc2789b8af2db19c22772847df244bd1d2cf0d0a
BossBaby25 <meraj15-4213@diu.edu.bd>
2022-09-30 12:49:04 +0600

    time adding done

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -3,25 +3,35 @@ import Subject from '../Subject/Subject';
 
 const Activity = () => {
     const [subjects, setSubjects] = useState([]);
+    const [exerciseTime,setExerciseTime]=useState(0);
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setSubjects(data))
     }, []);
+    const handleClick =(subject) => {
+        // console.log(subject);
+        // exerciseTime.push(subject);
+        // const newTime =[...exerciseTime,subject];
+        const newTime =exerciseTime+subject;
+        setExerciseTime(newTime);
+    }
     return (
         <div className='activity-container row'>
-            <div className="subjects-container col-8 ps-5">
-                <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="subjects-container col-lg-8 ps-5 col-sm-12">
+                <div className="row row-cols-1 row-cols-lg-3 row-cols-md-2 g-4">
                         {
                             subjects.map(subject => <Subject
                                 key={subject.id}
                                 subject={subject}
+                                handleClick={handleClick}
                             ></Subject>)
                         }
                 </div>
             </div>
-            <div className="details-container col-4">
-                <h1>details</h1>
+            <div className="details-container col-4 ps-5">
+                <h1>Exercise details: </h1>
+                <p>Exercise Time: {exerciseTime}</p>
             </div>
         </div>
     );
--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -3,14 +3,15 @@ import './Subject.css';
 
 const Subject = (props) => {
     const { img, name, time } = props.subject;
+    
     return (
         <div className='col'>
             <div className='card card-container mb-1 h-80'>
                 <img src={img} alt="" className='card-img-top' />
                 <div className="card-body">
                     <h5 className='card-title'>{name}</h5>
-                    <p className='card-text'>Target Duration: {time}</p>
-                    <button className='btn btn-secondary ps-5 pe-5'>Add to list</button>
+                    <p className='card-text'>Target Duration: {time}hr</p>
+                    <button onClick={() =>props.handleClick(props.subject.time)} className='btn btn-secondary ps-5 pe-5'>Add to list</button>
                 </div>
             </div>
         </div>
